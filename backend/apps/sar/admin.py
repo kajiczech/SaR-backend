da3e5e181b0639a6db06c1032283dc39316f89ae
karel.cech <carl.cech@gmail.com>
2019-04-28 17:45:54 +0200

    Add models to administration

--- backend/apps/sar/admin.py
+++ backend/apps/sar/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
+from .models import *
+
 
 def get_class_for_admin(model):
     # TODO: move this to some utils
@@ -12,3 +14,11 @@ def get_class_for_admin(model):
 
 def register_model(model):
     admin.site.register(model, get_class_for_admin(model))
+
+
+register_model(Geofence)
+register_model(Geotag)
+register_model(Invitation)
+register_model(Operation)
+register_model(OperationsAttendees)
+register_model(CheckedPoint)
