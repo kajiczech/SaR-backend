b422fcc5077bf4d935fa0bd888d27a65df92bfc9
karel.cech <carl.cech@gmail.com>
2019-08-04 13:59:42 +0200

    Uncomment basic serialization tess

--- backend/core/tests/testControllers.py
+++ backend/core/tests/testControllers.py
@@ -1,4 +1,4 @@
-from backend.apps.sar.models import *
+from backend.core.tests.models import *
 from backend.core.api.GenericViewSet import GenericViewSet
 from backend.core.tests.testGenericViewSet import BaseApiTest
 
@@ -9,16 +9,16 @@ class TestManyToManyController(BaseApiTest):
         self.view = GenericViewSet.as_view({'get': 'retrieve'})
 
     def test_basic_serialization(self):
-        pass
-        # operation = Operation.objects.create(type="flood", name="FirstOperation",
-        #                                      start_date="2019-04-06T14:43:56.630468Z"
-        #                                      )
-        # link = OperationsAttendees.objects.create(attendee=self.regular_user, operation=operation,
-        #                                           attendee_role='Organizer'
-        #                                           )
-        #
-        # response = self.view(self.get_request('get', user=self.admin_user), Model="OperationsAttendees", id=link.id)
-        #
-        # assert response.data["attendee"]['id'] == str(self.regular_user.id)
-        # assert response.data["operation"]['id'] == str(operation.id)
-        # assert response.data["attendee_role"] == 'Organizer'
+
+        operation = TestModel.objects.create(type="flood", name="FirstOperation",
+                                             start_date="2019-04-06T14:43:56.630468Z"
+                                             )
+        link = M2mLinkModel.objects.create(attendee=self.regular_user, operation=operation,
+                                           attendee_role='Organizer'
+                                           )
+
+        response = self.view(self.get_request('get', user=self.admin_user), Model="M2mLinkModels", id=link.id)
+
+        assert response.data["attendee"]['id'] == str(self.regular_user.id)
+        assert response.data["operation"]['id'] == str(operation.id)
+        assert response.data["attendee_role"] == 'Organizer'
