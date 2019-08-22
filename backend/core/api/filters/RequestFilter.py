766387cc5359488c1e9b56ac34be236eead517cb
karel.cech <carl.cech@gmail.com>
2019-08-22 19:46:16 +0200

    Support `and` and `or` operators in filter

--- backend/core/api/filters/RequestFilter.py
+++ backend/core/api/filters/RequestFilter.py
@@ -1,5 +1,6 @@
 import json
 
+from django.db.models import Q
 from rest_framework import filters
 
 
@@ -10,4 +11,32 @@ class RequestFilter(filters.BaseFilterBackend):
         if not filter_json:
             return queryset
         filter_def = json.loads(filter_json)
-        return queryset.filter(**filter_def)
+        query = self.get_query(filter_def)
+        return queryset.filter(query)
+
+    def get_query(self, filter_def, and_operand=True):
+        """
+        :param filter_def: Dictionary
+        :param and_operand: Bool whether the operand in query is `and` or `or` . Could not figure out better name
+        :return:
+        """
+        query = Q()
+        for key, value in filter_def.items():
+
+            if key.lower() == '$or':
+                child_query = self.get_query(value, False)
+            elif key.lower() == '$and':
+                child_query = self.get_query(value, True)
+            else:
+                child_query = Q(**{key: value})
+
+            if and_operand:
+                query &= child_query
+            else:
+                query |= child_query
+
+        return query
+
+
+
+
